66f6afa5094667eba1af61e8d775803a8170d84d
cwolas@gmail.com <cwolas@gmail.com@e2cb38cb-497d-b2b5-6d1f-bc03abe549e3>
2012-02-11 00:43:38 +0000

    Formatted the code a little. Known problem:
    	KPCA.py appears to not be working. Not sure if I'm missing a library or something...'wwWhAaT's up wit Dat???'.

--- trunk/src/svd.py
+++ trunk/src/svd.py
@@ -2,37 +2,42 @@
 Created on Jan 29, 2012
 
 @author: King Charles
-'''
+edit: cw 
 
+'''
 from scipy import linalg, mat;
 
-# Sorry it's not much of anything. Haven't had time this weekend. Blergh
+# code for svd
+def svd():
+	A = mat( [[1,0,0,0,2],
+          	[0,0,3,0,0],
+          	[0,0,0,0,0],
+          	[0,4,0,0,0]] )
 
-# test out svd library
+	print "================="
+	print "A:\n",  A
+	print "=================\n"
 
-A = mat( [[1,0,0,0,2],
-          [0,0,3,0,0],
-          [0,0,0,0,0],
-          [0,4,0,0,0]] )
+	# S for sigma
+	U, S, V = linalg.svd( A )
 
-print "================="
-print "A:\n",  A
-print "=================\n"
+	# This one does not match the worked out wikipedia tutorial. Hmm.
+	print "U is a matrix whose columns are the eigenvectors of the A*A^T matrix."
+	print "These are termed the left eigenvectors.\n"
+	print "U:\n", U, "\n"
 
-# S for sigma
-U, S, V = linalg.svd( A )
+	print "S is a matrix whose diagonal elements are the singular values of A."
+	print "This is a diagonal matrix, so its nondiagonal elements are zero by definition.\n"
+	print "Sigma:\n", S, "\n"
 
-# This one does not match the worked out wikipedia tutorial. Hmm.
-print "U is a matrix whose columns are the eigenvectors of the A*A^T matrix."
-print "These are termed the left eigenvectors.\n"
-print "U:\n", U, "\n"
+	print "V is a matrix whose columns are the eigenvectors of the ATA matrix."
+	print "These are termed the right eigenvectors. VT is the transpose of V.\n"
+	print "V^T:\n", V, "\n"
 
-print "S is a matrix whose diagonal elements are the singular values of A."
-print "This is a diagonal matrix, so its nondiagonal elements are zero by definition.\n"
-print "Sigma:\n", S, "\n"
+# main function used for test functions/classes
+def main():
+	svd()	
 
-print "V is a matrix whose columns are the eigenvectors of the ATA matrix."
-print "These are termed the right eigenvectors. VT is the transpose of V.\n"
-print "V^T:\n", V, "\n"
-
-# test out kpca library, TODO
+# main used for test cases: pust test calls in main()
+if __name__ == "__main__":
+	main()
