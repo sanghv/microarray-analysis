79245c3354a93f1340859ba24d2daf279310fbdd
albert.steppi@gmail.com <albert.steppi@gmail.com@e2cb38cb-497d-b2b5-6d1f-bc03abe549e3>
2012-03-10 21:50:57 +0000

    Started hacking together the kpca biplot from the components we already have. It doesn't quite work yet.

--- trunk/src/kpca_biplot.R
+++ trunk/src/kpca_biplot.R
@@ -1,8 +1,8 @@
-library(svd)
 library(kernlab)
+library(svd)
 
 # Take a matrix X and a parameter a and produce a list of the matrices G and H
-# that form its singular value decomposition as done in the paper.
+# that form its singular value decomposition G %*% t(H) as done in the paper.
 our.svd <- function(X, a) {
   decomp <- svd(X)
   U <- decomp$u
@@ -11,10 +11,10 @@ our.svd <- function(X, a) {
   return(list(G = U %*% D^a, H = V %*% D^(1-a)))
 }
 
-# Compute the kpca biplot, with the kernlab kpca package doing most of the work.
-# This function is not yet finished. I still need to add in the biplot part of
-# kpca biplot.
-kpca.biplot <- function(x, kernel = "rbfdot", kpar = list(sigma = 0.1),
+# Computes the svd of x, breaking it into the components G and H. Does kpca
+# on H and returns a list of G and H projected onto the principal
+# components.
+our.kpca <- function(x, kernel = "rbfdot", kpar = list(sigma = 0.1),
                         features = 0, th = 1e-4, na.action = na.omit,
                         alpha = 0.5, ...)
   {
@@ -22,15 +22,15 @@ kpca.biplot <- function(x, kernel = "rbfdot", kpar = list(sigma = 0.1),
     G <- x.svd$G
     H <- x.svd$H
 
-    res <- kpca(t(H), kernel, kpar, features, th, na.action)
+    res <- kpca(H, kernel, kpar, features, th, na.action)
     kernel <- do.call(kernel, kpar)
-    kg <- kernelMatrix(kernel, G, t(H))
-
+    kg <- kernelMatrix(kernel, H, G)
+  
     # Projects G onto the principal component values
-    G.prj <- kg %*% pcv(res)
+    G.prj <- t(pcv(res)) %*% kg
 
     # Right now it just returns a list that contains the rows of H and the rows
     # of G projected onto the principal components.
-    return(list(g = G.prj, h = rotated(res)))
+    return(list(micro.arrays = G.prj, gene.expressions = rotated(res)))
   }
     
